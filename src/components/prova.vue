40911dacdf404e92216264f141e2daca529b02e0
Federica Pozzer <federica.pozzer@gmail.com>
2023-03-01 15:04:15 +0100

    bootstrap ok - scss (hopefully) ok

--- src/components/prova.vue
+++ src/components/prova.vue
@@ -3,9 +3,20 @@
 
 <template>
     <h1>Init Ok</h1>
+    <div class="sono container">
+        <div class="pirla">
+            We
+        </div>
+    </div>
 </template>
 
 
 <style lang="scss" scoped>
+
+.sono {
+    .pirla {
+        color: red;
+    }
+}
 
 </style>
