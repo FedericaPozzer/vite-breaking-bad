57f5a6892d2d9d3896fabd1e7a47be0544c65447
Federica Pozzer <federica.pozzer@gmail.com>
2023-03-01 14:39:33 +0100

    init project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import prova from "./components/prova.vue"
+
+export default {
+  components : {
+    prova
+  }
+}
+
+</script>
+
+<template>
+  <prova></prova>
+</template>
+
+<style>
+
+</style>
